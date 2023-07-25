7e46d3fc82c9527a38635dda71fbf8454e0f45a0
qicosmos <qicosmos@163.com>
2023-07-25 11:19:49 +0800

    update and submodule

--- main.cpp
+++ main.cpp
@@ -70,23 +70,25 @@ inline int64_t get_time_stamp(std::string_view str) {
   return tm;
 }
 
-inline std::string read_table() {
+inline std::string read_table(const std::vector<std::string> &dbs) {
   std::string result;
-  std::vector<std::string> names{"yalantinglibs", "async_simple"};
-  for (auto &name : names) {
+  for (auto &name : dbs) {
     ormpp::dbng<ormpp::sqlite> sqlite;
     sqlite.connect((name + ".db").data());
     sqlite.create_datatable<clone_t>(ormpp_auto_key{"unix_time"});
     auto tp = sqlite.query<std::tuple<int>>("select sum(uniques) from clone_t");
 
-    result.append(name)
+    result.append("until ")
+        .append(cinatra::get_local_time_str())
+        .append(" ")
+        .append(name)
         .append(" unique clones: ")
         .append(std::to_string(std::get<0>(tp[0])))
         .append(", details:\n");
 
-    auto vec = sqlite.query<clone_t>();
+    auto vec = sqlite.query<clone_t>("order by unix_time desc", "limit 365");
     for (auto &item : vec) {
-      result.append(item.timestamp)
+      result.append(item.timestamp.substr(0, 10))
           .append(", ")
           .append(std::to_string(item.count))
           .append(", ")
@@ -114,7 +116,8 @@ void update_table(const std::vector<std::string> &args,
       std::string url = "https://api.github.com/repos/";
       url.append(repo).append("/traffic/clones");
       auto result = client.get(url);
-      if (result.status != 200) {
+      // if (result.status != 200)
+      {
         std::cerr << result.status << "\n";
         std::cerr << result.resp_body << "\n";
         std::cout << "will retry in 5 seconds\n";
@@ -152,37 +155,42 @@ void update_table(const std::vector<std::string> &args,
   }
 }
 
-// eg: ./github_stat your_token alibaba/yalantinglibs alibaba/async_simple
+// eg: ./github_stat your_github_token alibaba/yalantinglibs
 int main(int argc, char **argv) {
   using namespace cinatra;
-  cinatra::http_server server(8);
-  bool r = server.listen("0.0.0.0", "9988");
-  if (!r) {
-    std::cerr << "listen failed\n";
-    return -1;
-  }
-
-  server.set_http_handler<GET, POST>("/", [](request &, response &res) {
-    auto result = read_table();
-    res.set_status_and_content(status_type::ok, std::move(result));
-  });
-
-  std::thread svr_thd([&server] { server.run(); });
 
   if (argc < 3) {
     std::cerr << "lack of args: auth_token and repo_owner/repo_name\n";
-    std::cout
-        << "example: "
-           "glb_dgHKs4qDkjm89dsccydQZj0W3RkDcF6w7s8z, alibaba/yalantinglibs\n";
+    std::cout << "example: "
+                 "your_github_token, alibaba/yalantinglibs\n";
     return -1;
   }
 
   std::string auth_token = "Bearer ";
   auth_token.append(argv[1]);
   std::vector<std::string> repos;
+  std::vector<std::string> dbs;
   for (int i = 2; i < argc; i++) {
-    repos.push_back(argv[i]);
+    std::string repo = argv[i];
+    auto db_name = repo.substr(repo.find("/") + 1);
+    dbs.push_back(std::move(db_name));
+    repos.push_back(std::move(repo));
   }
+
+  cinatra::http_server server(8);
+  bool r = server.listen("0.0.0.0", "9980");
+  if (!r) {
+    std::cerr << "listen failed\n";
+    return -1;
+  }
+
+  server.set_http_handler<GET, POST>("/", [&dbs](request &, response &res) {
+    auto result = read_table(dbs);
+    res.set_status_and_content(status_type::ok, std::move(result));
+  });
+
+  std::thread svr_thd([&server] { server.run(); });
+  svr_thd.join();
 
   auto &pool = coro_io::g_io_context_pool(argc - 2);
 
